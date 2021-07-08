5ebc031b01d00dd3e2f57d92b7df3d7a35b3636c
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-07-08 15:41:21 +0200

    translate texts

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -6,4 +6,11 @@ return [
         'description' => 'Personalize your back-end by pinning your most used pages directly to the main menu.',
         'tab' => 'Customization',
     ],
+    'menu' => [
+        'item_tooltip' => 'Pinned pages',
+        'pin_current' => 'Pin this page',
+        'unpin_current' => 'Unpin this page',
+        'sidemenu_items_title' => 'Pinned Pages',
+        'sidemenu_item_remove' => 'Remove pin',
+    ],
 ];
